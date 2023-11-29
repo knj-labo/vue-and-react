01795a9b564c85d327ef9a54bf749d18cda76dce
jp-knj <kenji.tomita1996@gmail.com>
2023-11-29 23:19:13 +0900

    feat: add VueAppPage

---
+++ src/components/VueAppPage.tsx
@@ -0,0 +1,28 @@
+import { useEffect, FC } from 'react';
+
+import { VUE_APP_ID } from './vue-app-id';
+
+/**
+ * vue-app の表示非表示を制御するための page component.
+ */
+export const VueAppPage: FC = () => {
+    useEffect(() => {
+        showVueApp();
+        return () => {
+            hideVueApp();
+        };
+    }, []);
+    return null;
+};
+
+function showVueApp() {
+    const vueApp: HTMLElement | null = document.querySelector(`#${VUE_APP_ID}`);
+    if (!vueApp) return;
+    vueApp.hidden = false;
+}
+
+function hideVueApp() {
+    const vueApp: HTMLElement | null = document.querySelector(`#${VUE_APP_ID}`);
+    if (!vueApp) return;
+    vueApp.hidden = true;
+}
